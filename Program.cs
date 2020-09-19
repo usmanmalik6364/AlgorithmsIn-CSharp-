221b7595e5ecdca0fc37929ff3f237ae6b3f8948
Usman Malik <usmanmalik9797@gmail.com>
2020-09-19 13:38:50 +1000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DynamicConnectivity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = Convert.ToInt32( Console.ReadLine()); //convert standard input to Integer
+            //Uf uf = new Uf(N);
+            //while (!Convert.ToInt32(Console.ReadLine()).isEmpty())
+            //{
+
+            //}
+
+        }
+    }
+}
---
+++ QuickFindUF.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DynamicConnectivity
+{
+    /// <summary>
+    /// Eager Approach, can lead to quadratic time for larger problems.
+    /// QuickFind algorithm analysis
+    /// Initialise -> O(N)
+    /// Union -> O(N)
+    /// Find -> O(1)
+    /// </summary>
+    class QuickFindUF
+    {
+        private int[] id;
+        public QuickFindUF(int N)
+        {
+            id = new int[N];
+            for (int i = 0; i < N; i++)
+            {
+                id[i] = i;
+            }
+        }
+        public Boolean connected(int p, int q)
+        {
+            return (id[p] == id[q]);
+        }
+        public void union(int p, int q)
+        {
+            int pid = id[p];
+            int qid = id[q];
+            for (int i = 0; i < id.Length; i++)
+            {
+                if(id[i]==pid)
+                {
+                    id[i] = qid;
+                }
+            }
+        }
+    }
+}
